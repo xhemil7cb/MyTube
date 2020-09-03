fdcc4591f12136185eb3ca09c9b5a4ea6a7b29cd
Xhemil <xhemil7cb@gmail.com>
2020-09-03 05:43:26 +0200

    Swipe Added

---
+++ app/src/main/java/com/example/mytube/SomeActivity.java
@@ -0,0 +1,14 @@
+package com.example.mytube;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_some);
+    }
+}
